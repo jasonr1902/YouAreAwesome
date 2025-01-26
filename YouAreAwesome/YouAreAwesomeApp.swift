4856fd9114cba075a2d7383b02f3934c8d10e8e9
jasonr1902 <jason.royer1902@gmail.com>
2025-01-25 18:10:04 -0600

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Jason Royer on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
