1071f7f6be1c6450923af8d8206cb1145b1aad17
jasonr1902 <jason.royer1902@gmail.com>
2025-01-25 18:52:23 -0600

    Updates after first lesson

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,11 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            Image(systemName: "football")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.purple)
+            Text("You Are Awesome!")
+                .font(.title)
+                .fontWeight(.semibold)
+                .foregroundStyle(Color.purple)
+                .multilineTextAlignment(.trailing)
+                .padding(.vertical)
+            Text("Your Mom!")
+                .font(.largeTitle)
+                .padding(.all)
+                .foregroundStyle(.blue)
         }
         .padding()
     }
